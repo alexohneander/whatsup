4c3684004208ace1fb0ae0bc1bb2fce4383a119b
Alex Wellnitz <moin@wellnitz-alex.de>
2024-03-14 23:15:57 +0100

    feat: add admin users and authentication for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :jobs do
@@ -8,7 +9,9 @@ Rails.application.routes.draw do
   end
 
   # Sidekiq UI
-  mount Sidekiq::Web => '/admin/sidekiq'
+  authenticate :admin do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
