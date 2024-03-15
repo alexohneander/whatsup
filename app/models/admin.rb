12c9dc18b14d9de973cd8f19685690a76e7c7910
Alex Wellnitz <moin@wellnitz-alex.de>
2024-03-15 14:01:31 +0100

    test: activate register function for admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :registerable
-  devise :database_authenticatable, :trackable,
+  devise :database_authenticatable, :trackable, :registerable,
          :recoverable, :rememberable, :validatable
 end
